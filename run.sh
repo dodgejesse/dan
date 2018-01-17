1c66c92d18e2d9da5ae73fe9f54613872e33f466
Ubuntu <jessed@vmFromImage.zyq1h22la0oexmzxpotmiy5tyf.bx.internal.cloudapp.net>
2018-01-17 00:03:26 +0000

    small change

--- run.sh
+++ run.sh
@@ -6,16 +6,18 @@ mkdir -p models
 
 # Get trees
 loc=trainDevTestTrees_PTB.zip
-curl -O http://nlp.stanford.edu/sentiment/$loc
+curl -O https://nlp.stanford.edu/sentiment/$loc
 unzip $loc -d data
 mv data/trees data/sentiment
 rm -f $loc
 
 # Get Glove 300d embeddings
-curl -O http://www-nlp.stanford.edu/data/glove.840B.300d.zip
+curl -O https://nlp.stanford.edu/data/glove.840B.300d.zip
 mv glove.840B.300d.zip data/
 
 # Convert tree format
 cd preprocess
 python sentiment_trees.py
 python load_embeddings.py
+
+mv data/sentiment_We data/sentiment_all_We
